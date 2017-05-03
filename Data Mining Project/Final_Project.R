6b8314d5d4eb25c7f2e3d6c9d8e4043442c52acf
gappleDEV <gregoryjohnson008@gmail.com>
2017-05-03 15:30:07 -0400

    Presentation and Code addition
    Presentation finished and small code changes made to clean up and add
    info

--- Data Mining Project/Final_Project.R
+++ Data Mining Project/Final_Project.R
@@ -2,7 +2,7 @@
 # Final Project
 
 rm(list=ls(all=TRUE))
-drawPlots = TRUE
+options(warn=-1)
 set.seed(1234)
 
 # For predicting how the present state of the environment will change
@@ -15,14 +15,12 @@ numCols = ncol(climData)
 myRows <- c(1:numRows)
 
 # Line plot of all sets of data (columns)
-if(drawPlots) {
-  par(mfrow=c(3,3))
-  for(i in 4:numCols) { # skip index [1,2,3] because plotting would be pointless
-    heading = paste("",colnames(climData)[i])
-    temp <- as.vector(as.matrix(climData[i]))
-    plot(myRows, temp, main = heading, xlab="Month # in Data", ylab=colnames(climData)[i])
-    lines(myRows, temp, type="l")
-  }
+par(mfrow=c(3,3))
+for(i in 4:numCols) { # skip index [1,2,3] because plotting would be pointless
+  heading = paste("",colnames(climData)[i])
+  temp <- as.vector(as.matrix(climData[i]))
+  plot(myRows, temp, main = heading, xlab="Month # in Data", ylab=colnames(climData)[i])
+  lines(myRows, temp, type="l")
 }
 
 # For getting max, min and standard deviation of each column
@@ -34,34 +32,6 @@ colMax(climData[4:numCols])
 colMin(climData[4:numCols])
 colSd(climData[4:numCols])
 
-# Classify the wet and dry periods
-'classify <- c()
-for(j in 1:numRows) {
-  if(climData[j,7] >= 4.00) {
-    #classify <- c(classify, "Extreme wetness")
-    classify <- c(classify, 3)
-  } else if(climData[j,7] >= 3.00) {
-    #classify <- c(classify, "Severe wetness")
-    classify <- c(classify, 2)
-  } else if(climData[j,7] >= 1.50) {
-    #classify <- c(classify, "Mild to moderate wetness")
-    classify <- c(classify, 1)
-  } else if(climData[j,7] >= -1.49) {
-    #classify <- c(classify, "Near normal")
-    classify <- c(classify, 0)
-  } else if(climData[j,7] >= -2.99) {
-    #classify <- c(classify, "Mild to moderate drought")
-    classify <- c(classify, -1)
-  } else if(climData[j,7] >= -3.99) {
-    #classify <- c(classify, "Severe drought")
-    classify <- c(classify, -2)
-  } else {
-    #classify <- c(classify, "Extreme drought")
-    classify <- c(classify, -3)
-  }
-}
-climData["Wet/Dry Period"] <- classify'
-
 # Create test and training groups
 #   Take every 9th and put in test dataset
 #   Put the rest in training dataset
@@ -71,7 +41,6 @@ clippedData <- climData[,3:21]
 getMonth <- function(x) substr(x, 5, length(x))
 clippedData <- data.frame(apply(clippedData[1],2,getMonth), clippedData[2:19])
 clippedData$YearMonth <- as.numeric(clippedData$YearMonth)
-clippedData
 test<-clippedData[multsOf,]
 training<-clippedData[!multsOf,]
 
@@ -93,7 +62,18 @@ for(n in 1:10) {
   myResults <- knn(training[,-19],test[,-19],training[,19],k=n)
   print(paste("K =",n,":",checkPerformance(test, myResults)), quote=F)
 }
-
+'
+[1] K = 1 : 0.3125
+[1] K = 2 : 0.3125
+[1] K = 3 : 0.5
+[1] K = 4 : 0.375
+[1] K = 5 : 0.4375
+[1] K = 6 : 0.4375
+[1] K = 7 : 0.4375
+[1] K = 8 : 0.3125
+[1] K = 9 : 0.4375
+[1] K = 10 : 0.375
+'
 # K nearest neighbor averages about 44% (Between k=4 and k=5) correct on predictions using the current training and testing datasets
 
 # Artificial neural network-------------------------------------------------------------------
@@ -133,9 +113,7 @@ f
 # neuralnet creation
 library(neuralnet)
 nn <- neuralnet(f,train,hidden=10,linear.output=FALSE)
-if(drawPlots){ 
-  plot(nn) 
-}
+plot(nn)
 
 # Compute Predictions off Test Set
 pr.nn <- compute(nn,test[-1])
@@ -146,11 +124,12 @@ test.r <- (test$WetnessIndex)*(max(data$WetnessIndex)-min(data$WetnessIndex))+mi
 
 MSE.nn <- sum((test.r - pr.nn_)^2)/nrow(test)
 
-if(drawPlots) {
-  par(mfrow=c(1,1))
-  plot(test$WetnessIndex,pr.nn_,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
-  legend('bottomright',legend='NN',pch=18,col='red', bty='n')
-}
+par(mfrow=c(1,1))
+plot(test$WetnessIndex,pr.nn_,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
+lmRes <- lm(pr.nn_ ~ test$WetnessIndex)
+print(paste("Success Rate: ", (1 - abs(coef(lmRes)[2] - 1)) * 100, "%")) #slope
+abline(lmRes)
+legend('bottomright',legend='NN',pch=18,col='red', bty='n')
 
 table(test$WetnessIndex,pr.nn_)
 
@@ -174,24 +153,28 @@ table(myPredict, nbData$Wet.Dry.Period)
 library(party)
 
 input.data <- clippedData
-output.tree <- ctree(Wet.Dry.Period ~ ., data = input.data)
+target <- Wet.Dry.Period ~ .
+output.tree <- ctree(target, data = input.data)
 plot(output.tree)
+predDT <- predict(output.tree)
+table(predDT, input.data$Wet.Dry.Period)
+print(paste("DT success =",checkPerformance(input.data, predDT)))
 
 # Classification and Regression--------------------------------------------------
 library(caret)
 lm_model <- train(Wet.Dry.Period ~ ., data = clippedData, method = "lm") # linear regression model
 summary(lm_model)
 
-# Linear Discriminant Analysis with Jacknifed Prediction------------------------- Good results
+# Linear Discriminant Analysis------------------------------------------------- Good results
 library(MASS)
 fit <- lda(Wet.Dry.Period ~ ., data=clippedData, 
            na.action="na.omit", CV=TRUE)
 plot(clippedData$Wet.Dry.Period,fit$class, pch = 16, col='blue', main="Real vs Predicted LDA")
 abline(lm(fit$class ~ clippedData$Wet.Dry.Period))
  
-# Assess the accuracy of the prediction
-# percent correct for each category of G
+# Assess the accuracy of the prediction percent correct
 ct <- table(clippedData$Wet.Dry.Period, fit$class)
 diag(prop.table(ct, 1))
 # total percent correct
-sum(diag(prop.table(ct)))
+print(paste("Percent correct from data: ", sum(diag(prop.table(ct))) * 100, "%"))
+print(paste("Success Rate: ", (1 - abs(coef(lm(fit$class ~ clippedData$Wet.Dry.Period))[2] - 1)) * 100, "%")) #slope
